613742ebdfd0b338f1cbd87736527c2c0c4f1e29
rm <biffbangpow767@yahoo.com>
2017-08-11 15:02:29 -0700

    simplify my conditional statements so that I can practice working on buttons and not run into bugs.

--- js/scripts.js
+++ js/scripts.js
@@ -5,29 +5,12 @@ $(document).ready(function() {
   $("#PHP").hide();
   $("#Java").hide();
 
-  var question1 = $("#question1").val();
-  var question2 = $("#question2").val();
-  var question3 = $("question3").val();
-  var question4 = $("question4").val();
-  var question5 = $("question5").val();
+  var question4 = $("#question4").val();
 
-  var name = prompt("enter name");
-  alert(name + " you'd make a great decision attending Epicodus!")
-
-  if (question1 === "social" && question2 === "coffee" && question3 === "at-home" && question4 === "democrat" && question5 === "New-Wave") {
+  if (question4 === "democrat") {
     $("#Ruby").show();
-  } else if (question1 === "social" && question2 === "coffee" && question3 === "at-home" && question4 === "democrat" && question5 === "pop-Music") {
+  } else if (question4 === "republican") {
     $("#PHP").show();
-  } else if (question1 === "social" && question2 === "coffee" && question3 === "at-home" && question4 === "republican" && question5 === "New-Wave") {
-    $("#Java").show();
-  } else if (question1 === "social" && question2 === "coffee" && question3 === "theater" && question4 === "republican" && question5 === "New-Wave") {
-    $("#Ruby").show();
-  } else if (question1 === "social" && question2 === "water" && question3 === "theater" && question4 === "republican" && question5 === "New-Wave") {
-    $("#PHP").show();
-  } else if (question1 === "social" && question2 === "water" && question3 === "theater" && question4 === "republican" && question5 === "New-Wave") {
-    $("#Java").show();
-  } else if (question1 === "anti-social" && question2 === "water" && question3 === "theater" && question4 === "republican" && question5 === "New-Wave") {
-    $("#Ruby").show();
   } else {
     $("#Java").show();
   }
