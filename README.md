d9b8cbab5eae8d2b3ca6a9c90a6580df46700b6d
Guest User <Guest@Epicodus-8A.local>
2017-08-11 10:42:23 -0700

    site where all of the initial components are functioning

---
+++ README.md
@@ -0,0 +1,6 @@
+Application name: Choose your Track
+Author: Rebecca McIntosh
+Description: This project allows users to answer 5 questions to determine which computer programming track best suits their personality
+Setup: For viewing, clone the repository, in the "tracks" folder, open the index.html file in a web browser to begin the quiz.
+Technologies used: HTML, CSS, JavaScript, and jQuery
+Legal: Copyright(c) 2017 Rebecca McIntosh. Software is licensed under the MIT license.
---
+++ js/scripts.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+  $("#formOne").submit(function(event) {
+
+  $("#Ruby").hide();
+  $("#PHP").hide();
+  $("#Java").hide();
+  var question1 = $("#question1").val();
+  var question2 = $("#question2").val();
+  var question3 = $("question3").val();
+  var question4 = $("question4").val();
+  var question5 = $("question5").val();
+
+  if (question1 === "social" && question2 === "coffee" && question3 === "at-home" && question4 === "democrat" && question5 === "New-Wave") {
+    $("#Ruby").show();
+  } else if (question1 === "social" && question2 === "coffee" && question3 === "at-home" && question4 === "democrat" && question5 === "pop-Music") {
+    $("#PHP").show();
+  } else if (question1 === "social" && question2 === "coffee" && question3 === "at-home" && question4 === "republican" && question5 === "New-Wave") {
+    $("#Java").show();
+  } else if (question1 === "social" && question2 === "coffee" && question3 === "theater" && question4 === "republican" && question5 === "New-Wave") {
+    $("#Ruby").show();
+  } else if (question1 === "social" && question2 === "water" && question3 === "theater" && question4 === "republican" && question5 === "New-Wave") {
+    $("#PHP").show();
+  } else if (question1 === "social" && question2 === "water" && question3 === "theater" && question4 === "republican" && question5 === "New-Wave") {
+    $("#Java").show();
+  } else if (question1 === "anti-social" && question2 === "water" && question3 === "theater" && question4 === "republican" && question5 === "New-Wave") {
+    $("#Ruby").show();
+  } else {
+    $("#Java").show();
+  }
+  event.preventDefault();
+  });
+});
