8f10648bf586316a38b11774c28553aad163dcd0
rm <biffbangpow767@yahoo.com>
2017-08-11 16:18:52 -0700

    add a commit to the repository with the final draft minus the styling.

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,20 @@
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
+    $("#home").hide();
+    $("#Ruby").hide();
+    $("#PHP").hide();
+    $("#Java").hide();
+    var name = prompt("Enter your name");
+    alert(name + " you'd make a great decision attending Epicodus!")
+    var question4 = $("#question4").val();
 
-  $("#Ruby").hide();
-  $("#PHP").hide();
-  $("#Java").hide();
-
-  var question4 = $("#question4").val();
-
-  if (question4 === "democrat") {
-    $("#Ruby").show();
-  } else if (question4 === "republican") {
-    $("#PHP").show();
-  } else {
-    $("#Java").show();
-  }
-  event.preventDefault();
-});
+    if (question4 === "democrat") {
+      $("#Ruby").show();
+    } else if (question4 === "republican") {
+      $("#PHP").show();
+    } else {
+      $("#Java").show();
+    }
+    event.preventDefault();
+  });
 });
